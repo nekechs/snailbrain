441ccdaa05cec9d20f6cf76e4cc29286c9db907c
nekechs <nekechs.dev@gmail.com>
2023-01-15 20:41:15 -0500

    Started framework for requires_grad

--- src/engine/comp_graph.rs
+++ src/engine/comp_graph.rs
@@ -9,12 +9,12 @@ use std::rc::Rc;
 
 pub struct Variable<T> {
     nodes: Rc<T>,
-    dimension: Vec<usize>
+    dimension: Vec<usize>,
 }
 
-pub fn zeros(dim: &[usize]) -> Variable<Leaf> {
+pub fn zeros(dim: &[usize], requires_grad: bool) -> Variable<Leaf> {
     Variable {
-        nodes: Rc::from(Leaf {}),
+        nodes: Rc::from(Leaf {requires_grad}),
         dimension: Vec::from(dim)
     }
 }
@@ -31,7 +31,7 @@ impl <T> Variable <T> {
         };
         Variable {
             nodes: Rc::from(operation),
-            dimension: self.dimension.clone()
+            dimension: self.dimension.clone(),
         }
     }
 
@@ -46,51 +46,77 @@ impl <T> Variable <T> {
         };
         Variable {
             nodes: Rc::from(operation),
-            dimension: self.dimension.clone()
+            dimension: self.dimension.clone(),
         }
     }
 
-    pub fn mmul<R>(&self, rhs: &Variable<R>) -> Variable<Subtraction<T, R>> {
+    pub fn mm_mul<R>(&self, rhs: &Variable<R>) -> Variable<MatMatMul<T, R>> {
         /* 
         
         TODO: Add logic here that detects dimension mismatch for matrix multiplication.
 
         */
 
-        let operation = Subtraction {
+        if !(self.dimension.len() == 2 && rhs.dimension.len() == 2 && self.dimension[1] == rhs.dimension[0]) {
+            panic!("Error with matrix multiplication dimensions.");
+        }
+
+        let operation = MatMatMul {
             first_operand: self.nodes.clone(),
             second_operand: rhs.nodes.clone(),
         };
         Variable {
             nodes: Rc::from(operation),
-            dimension: self.dimension.clone()
+            dimension: vec![self.dimension[0], rhs.dimension[1]],
+        }
+    }
+
+    pub fn mv_mul<R>(&self, rhs: &Variable<R>) -> Variable<MatVecMul<T, R>> {
+        if !(self.dimension.len() == 2 && rhs.dimension.len() == 1 && self.dimension[1] == rhs.dimension[0]) {
+            panic!("Error with matrix vector multiplication dimensions.");
+        }
+
+        let operation = MatVecMul {
+            matrix: self.nodes.clone(),
+            vector: rhs.nodes.clone(),
+        };
+        Variable {
+            nodes: Rc::from(operation),
+            dimension: vec![self.dimension[0]],
         }
     }
 
     pub fn sigmoid(&self) -> Variable<Sigmoid<T>> {
         Variable {
             nodes: Rc::from( Sigmoid{input: self.nodes.clone()}),
-            dimension: self.dimension.clone()
+            dimension: self.dimension.clone(),
         }
     }
 
     pub fn tanh(&self) -> Variable<Sigmoid<T>> {
         Variable {
             nodes: Rc::from( Sigmoid{input: self.nodes.clone()}),
-            dimension: self.dimension.clone()
+            dimension: self.dimension.clone(),
         }
     }
 
     pub fn relu(&self) -> Variable<Sigmoid<T>> {
         Variable {
             nodes: Rc::from( Sigmoid{input: self.nodes.clone()}),
-            dimension: self.dimension.clone()
+            dimension: self.dimension.clone(),
         }
     }
 }
 
-pub struct Leaf {
 
+/* Intention here is to specify operations common to nodes */
+/* Examples: Checking to see if node requires_grad, serialization, etc. */
+pub trait Node {
+
+}
+
+pub struct Leaf {
+    requires_grad: bool
 }
 
 pub struct Addition<X, Y> {
@@ -106,6 +132,11 @@ pub struct Subtraction<X, Y> {
 pub struct MatMatMul<X, Y> {
     first_operand: Rc<X>,
     second_operand: Rc<Y>
+}
+
+pub struct MatVecMul<X, Y> {
+    matrix: Rc<X>,
+    vector: Rc<Y>
 }
 
 pub struct Sigmoid<X> {
